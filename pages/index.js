e15171dd546cdf9b2d2a9a406e5522ef900781c2
Maykom Douglas <maykom.sk9@gmail.com>
2025-04-07 09:19:15 -0300

    Update start

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,10 @@
-import { useState } from "react";
-
 function Home() {
   return (
     <div>
-      <h1>Home - Page</h1>
-      <Contador />
+      <h1>inicial</h1>
     </div>
   );
 }
 
-function Contador() {
- const [contador, setContador] = useState(1);
-
- function adicionarContador() {
-  setContador(contador + 1);
- }
-
- return (
-  <div>
-    <div>{contador}</div>
-    <button onClick={adicionarContador}>Adicionar</button>
-  </div>
- )
-}
 
 export default Home;
